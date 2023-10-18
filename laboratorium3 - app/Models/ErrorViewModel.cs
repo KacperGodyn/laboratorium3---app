d910387010b5478496d864ec58c460df1cf279e8
kacper.godyn <kacper.godyn@SF-16>
2023-10-18 12:27:44 +0200

    Add project files.

---
+++ laboratorium3 - app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace laboratorium3___app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
