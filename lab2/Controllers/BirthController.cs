26e56457789afe595ef864d725381f7dad038094
KacperGodyn <kacgod@icloud.com>
2023-11-07 19:22:16 +0100

    update

---
+++ lab2/Controllers/BirthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lab2.Models;
+using System;
+
+namespace lab2.Controllers
+{
+    public class BirthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Greet(Birth model)
+        {
+            if (model == null)
+            {
+                // Możesz dodać logikę lub zwrócić widok błędu
+                return View("Error");
+            }
+
+            if (model.IsValid())
+            {
+                return View(model);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+
+    }
+}
---
+++ lab2/Controllers/CalculatorController.cs
@@ -0,0 +1,32 @@
+﻿using lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab2.Controllers
+{
+    public enum Operators
+    {
+        add, sub, mul, div
+    }
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Result([FromForm] Calculator model)
+        {
+            if (!model.IsValid())
+            {
+                return View("Error");
+            }
+            return View(model);
+        }
+
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
